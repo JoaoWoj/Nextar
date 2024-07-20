76866be45b5a413fd00f22e2e52931a55d5f3734
João Wojciechowski <55636585+JoaoWoj@users.noreply.github.com>
2024-07-19 22:02:58 -0300

    Commit inicial

---
+++ src/main/java/com/nextar/NextarApplication.java
@@ -0,0 +1,13 @@
+package com.nextar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextar/NextarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
